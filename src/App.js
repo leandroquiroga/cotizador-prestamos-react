d6a666e7f402b64656d1f1ea6241341f1ed85a31
Leandro <leandroquiroga9514@gmail.com>
2021-12-06 16:41:36 -0300

    Init App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import Header from './components/header/Header';
+import Forms from './components/form/form';
+import Message from './components/message/Message';
+import Result from './components/result/Result';
+import Spinner from './components/spinner/Spinner';
 
-function App() {
+const App = () => {
+  const [mount, saveMount] = useState(0)
+  const [methodMount, saveMethodMount] = useState('');
+  const [total, totalPay] = useState(0);
+  const [load, loadSpinner] = useState(false)
+
+  // Load components
+  let component;
+  if (load) {
+    component = <Spinner />
+  } else if (total === 0) {
+      component = <Message />
+  } else {
+    component = <Result mount={mount}
+                        methodMount={methodMount}
+                        total={total}
+                />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-2 App">
+      <Header title="Cotizador de Prestamos" />
+      <section className="bg-white p-2 d-flex flex-column justify-content-center align-items-center">
+        <Forms
+          mount= {mount}
+          saveMount={saveMount}
+          methodMount={methodMount}
+          saveMethodMount={saveMethodMount}
+          // total={total}
+          totalPay={totalPay}
+          loadSpinner = {loadSpinner}
+        />
+      </section>
+      <section className="bg-white p-2 d-flex flex-column justify-content-center align-items-center">
+        {component}
+      </section>
     </div>
   );
 }
-
 export default App;
